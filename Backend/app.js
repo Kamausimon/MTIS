05df8455364e2bf062f5c0ecf839d04a6abeba2e
kamau simon <kamausimon217@gmail.com>
2024-11-10 15:30:27 +0300

    Implement API routes and server initialization with error handling

--- Backend/app.js
+++ Backend/app.js
@@ -23,14 +23,31 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
-app.use(express.json());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(hpp()); // Prevent http param pollution
+
+//ROUTES
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const orderRouter = require("./routes/orderRoutes");
+const supplierRouter = require("./routes/supplierRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
+const auditRouter = require("./routes/auditRoutes");
+
+//ROUTES MIDDLEWARE
+app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
 //ROUTES
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
+app.use("/api/v1/orders", orderRouter);
+app.use("/api/v1/suppliers", supplierRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/audits", auditRouter);
+
+module.exports = app;
---
+++ Backend/server.js
@@ -0,0 +1,55 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectToDatabase = require("./db");
+
+dotenv.config({ path: "./config.env" });
+
+//call the database connection
+const connect = async () => {
+  try {
+    await connectToDatabase();
+    console.log("Database connection successful");
+  } catch (err) {
+    console.log("Database connection failed");
+    Process.exit(1);
+  }
+};
+
+//get the port from the environment variable
+const port = process.env.PORT || 3000;
+
+//START THE SERVER
+let server;
+const startServer = async () => {
+  await connect();
+  server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+};
+
+startServer();
+
+//handle unhandled promise rejections
+process.on("uncaughtException", (err) => {
+  console.log(new Date().toISOString(), err.name, err.message);
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(new Date().toISOString(), err.name, err.message);
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
