23fc0a058aea89ddd75802c2de49e456a88afe50
kamau simon <kamausimon217@gmail.com>
2024-11-12 03:39:53 +0300

    Add category model with schema definition and export

---
+++ Backend/models/categoryModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  parent_id: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  children: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  tenant_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Tenant",
+    required: true,
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+Module.exports = mongoose.model("Category", categorySchema); // Category is the name of the model, categorySchema is the schema
