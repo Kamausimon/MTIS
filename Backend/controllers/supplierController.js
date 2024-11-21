44bc16ad8c742f80a26381b75747aafca7b1d220
kamau simon <kamausimon217@gmail.com>
2024-11-21 16:10:57 +0300

    Add supplier management features: implement CRUD operations in supplierController and secure routes with authentication and authorization

---
+++ Backend/controllers/supplierController.js
@@ -0,0 +1,127 @@
+const supplier = require("../models/supplierModel");
+const AppError = require("../utils/AppError");
+const dotenv = require("dotenv");
+
+exports.getAllSuppliers = async (req, res, next) => {
+  try {
+    const suppliers = await supplier.find();
+
+    if (!suppliers) {
+      return next(new AppError("No suppliers found", 404));
+    } //if no suppliers are found, return an error
+    res.status(200).json({
+      status: "success",
+      result: suppliers.length,
+      data: {
+        suppliers,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+exports.createSupplier = async (req, res, next) => {
+  try {
+    const newSupplier = await supplier.create({
+      name: req.body.name,
+      contact_name: req.body.contact_name,
+      phone: req.body.phone,
+      email: req.body.email,
+      address: req.body.address,
+      city: req.body.city,
+      website: req.body.website,
+      products: req.body.products,
+      businessCode: req.body.businessCode,
+      status: req.body.status,
+      notes: req.body.notes,
+    });
+
+    if (!newSupplier) {
+      return next(new AppError("Could not create supplier", 404));
+    }
+
+    //if supplier is created successfully, return success message
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        newSupplier,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+exports.getSupplier = async (req, res, next) => {
+  try {
+    const supplier = await supplier.findById(req.params.id);
+
+    if (!supplier) {
+      return next(new AppError("Supplier not found", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        supplier,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+exports.updateSupplier = async (req, res, next) => {
+  try {
+    const updatedSupplier = await supplier.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updatedSupplier) {
+      return next(new AppError("Supplier not found", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        updatedSupplier,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+exports.deleteSupplier = async (req, res, next) => {
+  try {
+    const supplier = await supplier.findByIdAndDelete(req.params.id);
+
+    if (!supplier) {
+      return next(new AppError("Supplier not found", 404));
+    }
+    res.status(201).json({
+      status: "success",
+      message: "supplier deleted successfully",
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
--- Backend/routes/supplierRouter.js
+++ Backend/routes/supplierRouter.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const supplierController = require("../controllers/supplierController");
+const authController = require("../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
 //ROUTES
-router.route("/").get(supplierController.getAllSuppliers); // Get all suppliers
-router.route("/").post(supplierController.createSupplier); // Create a new supplier
-router.route("/:id").get(supplierController.getSupplier); // Get a single supplier
-router.route("/:id").patch(supplierController.updateSupplier); // Update supplier
-router.route("/:id").delete(supplierController.deleteSupplier); // Delete supplier
+router
+  .route("/")
+  .get(authController.protectRoute, supplierController.getAllSuppliers); // Get all suppliers
+router
+  .route("/")
+  .post(
+    authController.protectRoute,
+    authController.restrictToAdmin,
+    supplierController.createSupplier
+  ); // Create a new supplier
+router
+  .route("/:id")
+  .get(authController.protectRoute, supplierController.getSupplier); // Get a single supplier
+router
+  .route("/:id")
+  .patch(authController.protectRoute, supplierController.updateSupplier); // Update supplier
+router
+  .route("/:id")
+  .delete(
+    authController.protectRoute,
+    authController.restrictToAdmin,
+    supplierController.deleteSupplier
+  ); // Delete supplier
 
 module.exports = router;
