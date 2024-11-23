3afcbd0e16e6832629567bbe240fe359ecdd0c2f
kamau simon <kamausimon217@gmail.com>
2024-11-23 14:29:06 +0300

    Add category management features: implement getAllCategories and createCategory methods in categoryController, and secure category routes with authentication and authorization

--- Backend/controllers/categoryController.js
+++ Backend/controllers/categoryController.js
@@ -1,2 +1,38 @@
 const Category = require("../models/categoryModel");
 const AppError = require("../utils/appError");
+const dotenv = require("dotenv");
+
+exports.getAllCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+
+    if (!categories) {
+      return next(new AppError("No categories found", 404));
+    }
+    res.status(200).json({
+      status: "success",
+      results: categories.length,
+      data: {
+        categories,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.createCategory = async (req, res, next) => {
+  try {
+    const newCategory = Category.create({});
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.getCategory = async (req, res, next) => {};
+exports.updateCategory = async (req, res, next) => {};
+exports.deleteCategory = async (req, res, next) => {};
--- Backend/routes/categoryRouter.js
+++ Backend/routes/categoryRouter.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const categoryController = require("../controllers/categoryController");
+const authController = require("../controllers/authController");
 
 const router = express.Router({ mergeParams: true });
 
 //ROUTES
-router.route("/").get(categoryController.getAllCategories); // Get all categories
-router.route("/").post(categoryController.createCategory); // Create a new category
-router.route("/:id").get(categoryController.getCategory); // Get a single category
-router.route("/:id").patch(categoryController.updateCategory); // Update category
-router.route("/:id").delete(categoryController.deleteCategory); // Delete category
+router
+  .route("/")
+  .get(authController.protectRoute, categoryController.getAllCategories); // Get all categories
+router
+  .route("/")
+  .post(authController.protectRoute, categoryController.createCategory); // Create a new category
+router
+  .route("/:id")
+  .get(authController.protectRoute, categoryController.getCategory); // Get a single category
+router
+  .route("/:id")
+  .patch(
+    authController.protectRoute,
+    authController.restrictToManager,
+    categoryController.updateCategory
+  ); // Update category
+router
+  .route("/:id")
+  .delete(
+    authController.protectRoute,
+    authController.restrictToAdmin,
+    categoryController.deleteCategory
+  ); // Delete category
 
 module.exports = router;
