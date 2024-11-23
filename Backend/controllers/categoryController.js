c382d9673fdb4a27ea92f5c5706ef5ceb331c151
kamau simon <kamausimon217@gmail.com>
2024-11-23 15:40:30 +0300

    Add category management features: implement createCategory, getCategory, updateCategory, and deleteCategory methods in categoryController with error handling

--- Backend/controllers/categoryController.js
+++ Backend/controllers/categoryController.js
@@ -25,7 +25,21 @@ exports.getAllCategories = async (req, res, next) => {
 };
 exports.createCategory = async (req, res, next) => {
   try {
-    const newCategory = Category.create({});
+    const newCategory = Category.create({
+      name: req.body.name,
+      description: req.body.description,
+      parent_id: req.body.parent_id,
+      children: req.body.children,
+      level: req.body.level,
+      businessCode: req.body.businessCode,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        newCategory,
+      },
+    });
   } catch (err) {
     res.status(400).json({
       status: "fail",
@@ -33,6 +47,61 @@ exports.createCategory = async (req, res, next) => {
     });
   }
 };
-exports.getCategory = async (req, res, next) => {};
-exports.updateCategory = async (req, res, next) => {};
-exports.deleteCategory = async (req, res, next) => {};
+exports.getCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return next(new AppError("No category found with that ID", 404));
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        category,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.updateCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!category) {
+      return next(new AppError("No category found with that ID", 404));
+    }
+    res.status(200).json({
+      status: "success",
+      data: {
+        category,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+exports.deleteCategory = async (req, res, next) => {
+  try {
+    await Category.findByIdAndDelete(req.params.id);
+    if (!category) {
+      return next(new AppError("No category found with that ID", 404));
+    }
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
