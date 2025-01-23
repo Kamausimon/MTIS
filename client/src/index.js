643e8ae3c2d3ef9cdddc9c1a55e371b623184bae
kamau simon <kamausimon217@gmail.com>
2025-01-24 00:39:34 +0300

    Remove dotenv configuration from index.js as it's no longer needed

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "./components/themeContext";
 import "./index.css";
 import App from "./App";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
