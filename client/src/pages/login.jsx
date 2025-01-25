f177fc7a9af88471955e9a02200c1e23730cadc6
kamau simon <kamausimon217@gmail.com>
2025-01-26 01:18:51 +0300

    Update token logging in login component for clarity

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
 
     try {
       const response = await axios.post( `${url}/api/v1/users/login`, formData);
-      console.log('token', response)
+      console.log('token1', response)
       console.log('token from login 1', response.data.data.token);
       console.log('token from login', response.data.token);
       localStorage.setItem('token', response.data.token);
