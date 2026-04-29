b0d51482bbfa530dec393528b9193aaacc9fef09
kamau simon <99491328+Kamausimon@users.noreply.github.com>
2026-04-30 00:13:44 +0300

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 
 
-const DB = `mongodb+srv://kamausimon:${process.env.DATABASE_PASSWORD}@cluster1.iwigemq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+const DB = `mongodb+srv://kamausimon:${process.env.DATABASE_PASSWORD}@cluster1.iwigemq.mongodb.net/?appName=Cluster1`;
 const connectToDatabase = async () => {
   await mongoose.connect(DB);
 };
